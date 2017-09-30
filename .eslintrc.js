2cf639fb778a70c47d744015c9f7adf1eaf85856
Kunall Banerjee <yeskunall96@gmail.com>
2017-09-30 03:26:58 -0400

    :tada: init

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": ["airbnb-base", "prettier"],
+    "plugins": [
+        "import",
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "warn"
+      }
+};
