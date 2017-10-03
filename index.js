96476732f1536b33806b7331118edb5816eed190
Kunall Banerjee <yeskunall96@gmail.com>
2017-10-03 19:01:52 -0400

    add util methods

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const ONE_MINUTE = 60 * ONE_SECOND;
 const ONE_HOUR = 60 * ONE_MINUTE;
 const ONE_DAY = 24 * ONE_HOUR;
 
+const seconds = x => x * ONE_SECOND;
+const minutes = x => x * ONE_MINUTE;
+const hours = x => x * ONE_HOUR;
+const days = x => x * ONE_DAY;
+
 module.exports = {
   ONE_SECOND,
   ONE_MINUTE,
   ONE_HOUR,
   ONE_DAY,
+  seconds,
+  minutes,
+  hours,
+  days,
 };
